1c4d08c7f457052787446a0f22e2752d510254fe
Chirag Singla <chiragsingla@zohomail.in>
2025-09-01 15:48:52 +0530

    fix build errors on vercel

--- app/client/products/page.tsx
+++ app/client/products/page.tsx
@@ -6,3 +6,5 @@ export default async function Page() {
 
   return <ClientProducts products={products} />;
 }
+
+export const revalidate = 0; // ISR off, always dynamic
--- lib/actions.ts
+++ lib/actions.ts
@@ -1,15 +1,16 @@
 import { revalidatePath } from "next/cache";
 import { Prisma, Product } from "./generated/prisma/index";
+import { baseUrl } from '@/lib/constants'
 
 export async function getProducts() {
-  const response = await fetch("http://localhost:3000/api/admin/products");
+  const response = await fetch(`${baseUrl}api/admin/products`);
   const products = (await response.json()) as any as Product[];
   
   return products
 }
 
 export async function updateProduct(data: Prisma.ProductCreateInput) {
-  await fetch("http://localhost:3000/api/admin/products/product", {
+  await fetch(`${baseUrl}/api/admin/products/product`, {
     method: "POST",
     body: JSON.stringify(data)
   })
