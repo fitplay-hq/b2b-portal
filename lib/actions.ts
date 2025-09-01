69eac4515ba462ee47e9c694429e4a870622e564
Chirag Singla <chiragsingla@zohomail.in>
2025-09-01 16:18:56 +0530

    fix baseUrl

--- lib/actions.ts
+++ lib/actions.ts
@@ -3,7 +3,7 @@ import { Prisma, Product } from "./generated/prisma/index";
 import { baseUrl } from '@/lib/constants'
 
 export async function getProducts() {
-  const response = await fetch(`${baseUrl}api/admin/products`);
+  const response = await fetch(`${baseUrl}/api/admin/products`);
   const products = (await response.json()) as any as Product[];
   
   return products
