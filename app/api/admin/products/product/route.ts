1dbf5525a88948c4eb4a5297c93b92143cd6d252
Chirag Singla <chiragsingla@zohomail.in>
2025-09-02 17:11:28 +0530

    fix api request body parsing

--- app/api/admin/products/product/route.ts
+++ app/api/admin/products/product/route.ts
@@ -2,150 +2,138 @@ import prisma from "@/lib/prisma";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 import { Category } from "@/lib/generated/prisma";
-import {z} from "zod";
 import { auth } from "@/app/api/auth/[...nextauth]/route";
 
-const CreateProductSchema = z.object({
-    id: z.string().uuid(),
-    name: z.string().min(2).max(100),
-    images: z.array(z.string().url()),
-    price: z.number().min(0),
-    sku: z.string().min(2).max(100),
-    availableStock: z.number().min(0),
-    description: z.string().min(10).max(1000),
-    specification: z.record(z.string(), z.any()),
-    categories: z.enum(Category),
-    avgRating: z.number().min(0).optional(),
-    noOfReviews: z.number().min(0).optional(),
-    brand: z.string().min(2).max(100),
-});
+import {
+  ProductCreateInputObjectSchema,
+  ProductUpdateInputObjectSchema,
+} from "@/prisma/generated/schemas";
 
 export async function GET(req: NextRequest) {
-    try {
-        const session = await getServerSession(auth);
+  try {
+    const session = await getServerSession(auth);
 
-        if (!session || !session?.user || session?.user?.role !== "ADMIN") {
-            return NextResponse.json(
-                { error: "Unauthorized" },
-                { status: 401 }
-            );
-        }
-
-        const id = req.nextUrl.searchParams.get("id");
-        if (!id) {
-            return NextResponse.json({ error: "Invalid Product ID" }, { status: 400 });
-        }
-        const product = await prisma.product.findUnique({
-            where: { id: id },
-        });
-        if (!product) {
-            return NextResponse.json({ error: "Product not found" }, { status: 404 });
-        }
-        return NextResponse.json(product);
-    } catch (error: any) {
-        return NextResponse.json(
-            { error: error.message || "Something went wrong" },
-            { status: 500 }
-        );
+    if (!session || !session?.user || session?.user?.role !== "ADMIN") {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
     }
+
+    const id = req.nextUrl.searchParams.get("id");
+    if (!id) {
+      return NextResponse.json(
+        { error: "Invalid Product ID" },
+        { status: 400 },
+      );
+    }
+    const product = await prisma.product.findUnique({
+      where: { id: id },
+    });
+    if (!product) {
+      return NextResponse.json({ error: "Product not found" }, { status: 404 });
+    }
+    return NextResponse.json(product);
+  } catch (error: any) {
+    return NextResponse.json(
+      { error: error.message || "Something went wrong" },
+      { status: 500 },
+    );
+  }
 }
 
 export async function POST(req: NextRequest) {
-    try {
-        const session = await getServerSession(auth);
+  try {
+    const session = await getServerSession(auth);
 
-        if (!session || !session?.user || session?.user?.role !== "ADMIN") {
-            return NextResponse.json(
-                { error: "Unauthorized" },
-                { status: 401 }
-            );
-        }
-
-
-        const body = await req.json();
-        const result = CreateProductSchema.safeParse(body);
-
-        if (!result.success) {
-            return NextResponse.json(
-                { error: result.error.format() },
-                { status: 400 }
-            );
-        }
-
-        const product = await prisma.product.create({
-            data: result.data,
-        });
-
-        return NextResponse.json(product, { status: 201 });
-    } catch (error: any) {
-        return NextResponse.json(
-            { error: error.message || "Something went wrong" },
-            { status: 500 }
-        );
+    if (!session || !session?.user || session?.user?.role !== "ADMIN") {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
     }
+
+    const body = await req.json();
+    const result = ProductCreateInputObjectSchema.safeParse(body);
+
+    if (!result.success) {
+      return NextResponse.json(
+        { error: result.error.format() },
+        { status: 400 },
+      );
+    }
+
+    const product = await prisma.product.create({
+      data: result.data,
+    });
+
+    return NextResponse.json(product, { status: 201 });
+  } catch (error: any) {
+    return NextResponse.json(
+      { error: error.message || "Something went wrong" },
+      { status: 500 },
+    );
+  }
 }
 
 export async function PATCH(req: NextRequest) {
-    try {
-        const session = await getServerSession(auth);
+  try {
+    const session = await getServerSession(auth);
 
-        if (!session || !session?.user || session?.user?.role !== "ADMIN") {
-            return NextResponse.json(
-                { error: "Unauthorized" },
-                { status: 401 }
-            );
-        }
-
-        const body = await req.json();
-        const result = CreateProductSchema.safeParse(body);
-
-        if (!result.success) {
-            return NextResponse.json(
-                { error: result.error.format() },
-                { status: 400 }
-            );
-        }
-
-        const product = await prisma.product.update({
-            where: { id: result.data.id },
-            data: result.data,
-        });
-
-        return NextResponse.json(product);
-    } catch (error: any) {
-        return NextResponse.json(
-            { error: error.message || "Something went wrong" },
-            { status: 500 }
-        );
+    if (!session || !session?.user || session?.user?.role !== "ADMIN") {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
     }
+
+    const body = await req.json();
+    const result = ProductUpdateInputObjectSchema.safeParse(body);
+
+    if (!result.success) {
+      return NextResponse.json(
+        { error: result.error.format() },
+        { status: 400 },
+      );
+    }
+
+    if (!result.data.id) {
+      return NextResponse.json(
+        { error: "product ID not supplied" },
+        { status: 400 }
+      )
+    }
+
+    const product = await prisma.product.update({
+      where: { id: result.data.id.toString() },
+      data: result.data,
+    });
+
+    return NextResponse.json(product);
+  } catch (error: any) {
+    return NextResponse.json(
+      { error: error.message || "Something went wrong" },
+      { status: 500 },
+    );
+  }
 }
 
 export async function DELETE(req: NextRequest) {
-    try {
-        const session = await getServerSession(auth);
+  try {
+    const session = await getServerSession(auth);
 
-        if (!session || !session?.user || session?.user?.role !== "ADMIN") {
-            return NextResponse.json(
-                { error: "Unauthorized" },
-                { status: 401 }
-            );
-        }
-
-        const id = req.nextUrl.searchParams.get("id");
-        if (!id) {
-            return NextResponse.json({ error: "Invalid Product ID" }, { status: 400 });
-        }
-
-        await prisma.product.delete({
-            where: { id: id },
-        });
-
-        return NextResponse.json({ message: "Product deleted successfully" });
-    } catch (error: any) {
-        return NextResponse.json(
-            { error: error.message || "Something went wrong" },
-            { status: 500 }
-        );
+    if (!session || !session?.user || session?.user?.role !== "ADMIN") {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
     }
-}
 
+    const id = req.nextUrl.searchParams.get("id");
+    if (!id) {
+      return NextResponse.json(
+        { error: "Invalid Product ID" },
+        { status: 400 },
+      );
+    }
+
+    await prisma.product.delete({
+      where: { id: id },
+    });
+
+    return NextResponse.json({ message: "Product deleted successfully" });
+  } catch (error: any) {
+    return NextResponse.json(
+      { error: error.message || "Something went wrong" },
+      { status: 500 },
+    );
+  }
+}
--- app/api/admin/products/route.ts
+++ app/api/admin/products/route.ts
@@ -2,47 +2,28 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 import { v4 as uuidv4 } from "uuid";
-import { $Enums, Category } from "@/lib/generated/prisma";
+import { $Enums, Prisma } from "@/lib/generated/prisma";
 import { getServerSession } from "next-auth";
 import { auth } from "../../auth/[...nextauth]/route";
-
-interface Product {
-  id: string;
-  name: string;
-  images: string[];
-  price: number;
-  sku: string;
-  availableStock: number;
-  description: string;
-  specification: Record<string, any>;
-  categories: Category;
-  avgRating: number;
-  noOfReviews: number;
-  brand: string;
-}
 
 export async function POST(req: NextRequest) {
   try {
     const body = await req.json();
 
     const session = await getServerSession(auth);
-    console.log("Session:", session);
 
     if (!session || !session?.user || session?.user?.role !== "ADMIN") {
-      return NextResponse.json(
-        { error: "Unauthorized" },
-        { status: 401 }
-      );
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
     }
 
     if (!Array.isArray(body)) {
       return NextResponse.json(
         { error: "Expected an array of products" },
-        { status: 400 }
+        { status: 400 },
       );
     }
 
-    const productsData: Product[] = body.map((p) => ({
+    const productsData: Prisma.ProductCreateInput[] = body.map((p) => ({
       id: uuidv4(),
       name: p.name,
       images: p.images ?? [],
@@ -64,12 +45,12 @@ export async function POST(req: NextRequest) {
 
     return NextResponse.json(
       { message: "Products added successfully", count: products.count },
-      { status: 201 }
+      { status: 201 },
     );
   } catch (error: any) {
     return NextResponse.json(
       { error: error.message || "Something went wrong" },
-      { status: 500 }
+      { status: 500 },
     );
   }
 }
@@ -79,17 +60,14 @@ export async function GET(req: NextRequest) {
     const session = await getServerSession(auth);
 
     if (!session || !session?.user || session?.user?.role !== "ADMIN") {
-      return NextResponse.json(
-        { error: "Unauthorized" },
-        { status: 401 }
-      );
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
     }
     const products = await prisma.product.findMany();
     return NextResponse.json(products);
   } catch (error: any) {
     return NextResponse.json(
       { error: error.message || "Something went wrong" },
-      { status: 500 }
+      { status: 500 },
     );
   }
 }
