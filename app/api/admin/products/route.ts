53ca97c2a706cdf5e34c671a023fd2ac605dae22
ayushcodes1729 <ayushtiwari0803@gmail.com>
2025-09-09 01:52:32 +0530

    feat:Bulk Inventory update by admin

--- app/api/admin/products/route.ts
+++ app/api/admin/products/route.ts
@@ -8,6 +8,13 @@ import { auth } from "../../auth/[...nextauth]/route";
 
 // Import the auto-generated Zod schema
 import { ProductCreateInputObjectSchema } from "@/prisma/generated/schemas";
+import z from "zod";
+const InventoryUpdateSchema = z.object({
+  productId: z.string().uuid(),
+  quantity: z.number().positive(),
+  direction: z.enum(["incr", "dec"]),
+  inventoryUpdateReason: z.enum(["NEW_PURCHASE", "PHYSICAL_STOCK_CHECK", "RETURN_FROM_PREVIOUS_DISPATCH"]),
+});
 
 export async function POST(req: NextRequest) {
   try {
@@ -69,6 +76,59 @@ export async function GET(req: NextRequest) {
 
     const products = await prisma.product.findMany();
     return NextResponse.json(products);
+  } catch (error: any) {
+    return NextResponse.json(
+      { error: error.message || "Something went wrong" },
+      { status: 500 },
+    );
+  }
+}
+
+
+// api for bulk inventory update
+export async function PATCH(req: NextRequest) {
+  try {
+    const body = await req.json();
+
+    const session = await getServerSession(auth);
+    if (!session || !session.user || session.user.role !== "ADMIN") {
+      return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    }
+
+    if (!Array.isArray(body)) {
+      return NextResponse.json(
+        { error: "Expected an array of inventory updates" },
+        { status: 400 },
+      );
+    }
+
+    const updates = body.map((p, idx) => {
+      const result = InventoryUpdateSchema.safeParse(p);
+      if (!result.success) {
+        throw new Error(
+          `Validation error at index ${idx}: ${JSON.stringify(
+            result.error.format(),
+          )}`,
+        );
+      }
+      return result.data;
+    });
+
+    const updatedProducts = await Promise.all(
+      updates.map(async ({ productId, quantity, direction, inventoryUpdateReason }) => {
+        return prisma.product.update({
+          where: { id: productId },
+          data: {
+            availableStock: {
+              [direction === "incr" ? "increment" : "decrement"]: quantity,
+            },
+            inventoryUpdateReason: inventoryUpdateReason,
+          },
+        });
+      })
+    );
+
+    return NextResponse.json({ success: true, updatedProducts, message: "Inventories of the listed products updated successfully" }, { status: 200 });
   } catch (error: any) {
     return NextResponse.json(
       { error: error.message || "Something went wrong" },
